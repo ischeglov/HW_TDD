a58b8f92dd08f4bad559454ed22d5330e192f14c
ischeglov <Ivan_mix@mail.ru>
2023-12-08 21:08:11 +0500

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,14 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
 
+    public static Map<String, String> book = new TreeMap<>();
+
     public int add(String name, String phone) {
-        return 0;
+        if (!book.containsKey(name)) {
+            book.put(name, phone);
+        }
+        return book.size();
     }
 }
